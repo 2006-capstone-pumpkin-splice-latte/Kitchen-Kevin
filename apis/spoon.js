166ebb37fc6d250626b9f44059093de2acb526e5
Clark Chen <yyunqingchenn@gmail.com>
2020-08-26 12:09:47 -0400

    spoonacular API more accurate results

--- apis/spoon.js
+++ apis/spoon.js
@@ -1,27 +1,26 @@
 const axios = require('axios');
 
 // 'apples%2Cflour%2Csugar'
-function spoonacularAPI(arrOfIngr) {
+function spoonacularAPI(arrOfIngr, count = 0) {
 	return axios({
-		method  : 'GET',
-		url     : 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients',
-		headers : {
-			'content-type'    : 'application/octet-stream',
-			'x-rapidapi-host' : 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-			'x-rapidapi-key'  : '6bbecd6a8bmshebec736d8c314e5p1b8dacjsnb819d756b83a',
-			useQueryString    : true
-		},
-		params  : {
-			number       : '1',
-			ranking      : '1',
-			ignorePantry : 'false',
-			ingredients  : arrOfIngr
-		}
-	})
+    "method":"GET",
+    "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    "x-rapidapi-key":"6bbecd6a8bmshebec736d8c314e5p1b8dacjsnb819d756b83a",
+    "useQueryString":true
+    },"params":{
+    "number":"5",
+    "cuisine": "korean",
+    "instructionsRequired":"true",
+    "query": arrOfIngr.join(', ')
+    }
+    })
 		.then((response) => {
 			return axios({
 				method  : 'GET',
-				url     : `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${response.data[0]
+				url     : `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${response.data[count]
 					.id}/information`,
 				headers : {
 					'content-type'    : 'application/octet-stream',

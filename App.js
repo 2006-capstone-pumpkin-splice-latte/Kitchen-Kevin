d430cb0204c40e36d7c9730336be55ebedaa53c4
Anderson Yoon <snowglow22y@gmail.com>
2020-08-27 11:07:36 -0400

    feat(bottom-tab-navigation)

--- App.js
+++ App.js
@@ -4,11 +4,20 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import Voice from "@react-native-community/voice";
 import Tts from "react-native-tts";
 import spoonacularAPI from "./apis/spoon";
-
 import { Dialogflow_V2 } from "react-native-dialogflow";
 import dfConfig from "./apis/config/dialogflowConfig";
 
-export default class App extends React.Component {
+
+
+
+import {createAppContainer} from 'react-navigation'
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs'
+import {Icon} from 'react-native-elements'
+
+import HomeScreen from './Screens/HomeScreen'
+
+
+class App extends React.Component {
   constructor(props) {
     super(props);
     Dialogflow_V2.setConfiguration(
@@ -180,3 +189,38 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
 });
+
+const TabNavigator=createMaterialBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: 'Home',
+        activeColor: 'dodgerblue',
+        inactiveColor: 'gray',
+        barStyle: {backgroundColor:'pink'},
+        tabBarIcon:()=>(
+          <View>
+            <Icon name={'home'} size={25} style={{color: 'black'}}/>
+          </View>
+        )
+      }
+    },
+    Test: {
+      screen: App,
+      navigationOptions: {
+        tabBarLabel: 'Test',
+        activeColor: 'dodgerblue',
+        inactiveColor: 'gray',
+        barStyle: {backgroundColor:'pink'},
+        tabBarIcon: () =>(
+          <View>
+            <Icon name={'person'} size={25} style={{color:'black'}}/>
+          </View>
+        )
+      }
+    }
+  }
+)
+
+export default createAppContainer(TabNavigator)

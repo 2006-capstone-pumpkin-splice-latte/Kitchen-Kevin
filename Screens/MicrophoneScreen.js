132939c9874df62132b0eb51d7b17be1c24db1bf
Adam Sue <adam.p.sue@gmail.com>
2020-08-31 19:45:19 -0400

    added bulleted list

--- Screens/MicrophoneScreen.js
+++ Screens/MicrophoneScreen.js
@@ -1,256 +1,268 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Button, ScrollView } from 'react-native';
-import Voice from '@react-native-community/voice';
-import Tts from 'react-native-tts';
-import spoonacularAPI from '../apis/spoon';
-import { Dialogflow_V2 } from 'react-native-dialogflow';
-import dfConfig from '../apis/config/dialogflowConfig';
-import { NavigationInjectedProps, withNavigation } from 'react-navigation';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  ScrollView,
+} from "react-native";
+import Voice from "@react-native-community/voice";
+import Tts from "react-native-tts";
+import spoonacularAPI from "../apis/spoon";
+import { Dialogflow_V2 } from "react-native-dialogflow";
+import dfConfig from "../apis/config/dialogflowConfig";
+import { NavigationInjectedProps, withNavigation } from "react-navigation";
 
 export default class MicrophoneScreen extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		Dialogflow_V2.setConfiguration(
-			dfConfig.client_email,
-			dfConfig.private_key,
-			Dialogflow_V2.LANG_ENGLISH,
-			dfConfig.project_id
-		);
-		this.state = {
-			results           : [],
-			ingredientsArr    : [],
-			allIngredientsArr : [],
-			recipeImage       : '',
-			recipeSteps       : [],
-			stepCount         : 0,
-			recipeCounter     : 0,
-			recipeTitle       : '',
-			ttsConfig         : {
-				iosVoiceId : 'com.apple.ttsbundle.Daniel-compact',
-				rate       : 0.5
-			},
-			screenHeight      : 0
-		};
-		// Voice.onSpeechResults = (result) => {
-		//   clearTimeout(Dialogflow_V2.silenceTimer)
-		//   if (result.value) {
-		//     Dialogflow_V2.speechResult = result.value;
-		//   }
-		//   Dialogflow_V2.silenceTimer = setTimeout(() => {
-		//     Voice.stop()
-		//   }, 1500)
-		// }
-		Tts.addEventListener('tts-start', (event) => {
-			console.log('TTS STARTED HAHAHAA');
-			Voice.stop();
-		});
-		Tts.addEventListener('tts-finish', (event) => {
-			console.log('TTS FINISHED HAHAHAA');
-			Voice.start();
-		});
-	}
+    Dialogflow_V2.setConfiguration(
+      dfConfig.client_email,
+      dfConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH,
+      dfConfig.project_id
+    );
+    this.state = {
+      results: [],
+      ingredientsArr: [],
+      allIngredientsArr: [],
+      recipeImage: "",
+      recipeSteps: [],
+      stepCount: 0,
+      recipeCounter: 0,
+      recipeTitle: "",
+      ttsConfig: {
+        iosVoiceId: "com.apple.ttsbundle.Daniel-compact",
+        rate: 0.5,
+      },
+      screenHeight: 0,
+    };
+    // Voice.onSpeechResults = (result) => {
+    //   clearTimeout(Dialogflow_V2.silenceTimer)
+    //   if (result.value) {
+    //     Dialogflow_V2.speechResult = result.value;
+    //   }
+    //   Dialogflow_V2.silenceTimer = setTimeout(() => {
+    //     Voice.stop()
+    //   }, 1500)
+    // }
+    Tts.addEventListener("tts-start", (event) => {
+      console.log("TTS STARTED HAHAHAA");
+      Voice.stop();
+    });
+    Tts.addEventListener("tts-finish", (event) => {
+      console.log("TTS FINISHED HAHAHAA");
+      Voice.start();
+    });
+  }
 
-	pullNames(array) {
-		let newArray = [];
-		for (let i = 0; i < array.length; i++) {
-			let currentElement = array[i];
-			newArray.push(currentElement.name);
-		}
-		return newArray;
-	}
-	initiateConversation() {
-		console.log('running init conversation');
-		Dialogflow_V2.startListening(
-			async (result) => {
-				let intent = result.queryResult.intent.displayName;
-				let response = result.queryResult.fulfillmentText;
-				this.setState({
-					results : [ ...this.state.results, `You: ${result.queryResult.queryText}` ]
-				});
-				if (result.queryResult.parameters.foodIngredients) {
-					this.setState({
-						ingredientsArr : result.queryResult.parameters.foodIngredients,
-						results        : [ ...this.state.results, `Kevin: ${response}` ]
-					});
-					Tts.speak(response, this.state.ttsConfig);
-				}
-				else if (intent === 'give-ingredients - yes') {
-					this.setState({
-						results : [ ...this.state.results, `Kevin: ${response}` ]
-					});
-					Tts.speak(response, this.state.ttsConfig);
+  pullNames(array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+      let currentElement = array[i];
+      newArray.push(currentElement.name);
+    }
+    return newArray;
+  }
+  initiateConversation() {
+    console.log("running init conversation");
+    Dialogflow_V2.startListening(
+      async (result) => {
+        let intent = result.queryResult.intent.displayName;
+        let response = result.queryResult.fulfillmentText;
+        this.setState({
+          results: [
+            ...this.state.results,
+            `You: ${result.queryResult.queryText}`,
+          ],
+        });
+        if (result.queryResult.parameters.foodIngredients) {
+          this.setState({
+            ingredientsArr: result.queryResult.parameters.foodIngredients,
+            results: [...this.state.results, `Kevin: ${response}`],
+          });
+          Tts.speak(response, this.state.ttsConfig);
+        } else if (intent === "give-ingredients - yes") {
+          this.setState({
+            results: [...this.state.results, `Kevin: ${response}`],
+          });
+          Tts.speak(response, this.state.ttsConfig);
 
-					const { data } = await spoonacularAPI(this.state.ingredientsArr);
+          const { data } = await spoonacularAPI(this.state.ingredientsArr);
 
-					let allIngredients = this.pullNames(data.extendedIngredients);
-					let sentence = `I got a recipe for ${data.title}. You will also need ${allIngredients.join(
-						', '
-					)}. Would you like to proceed with this recipe or should I find a new recipe?`;
+          let allIngredients = this.pullNames(data.extendedIngredients);
+          let sentence = `I got a recipe for ${
+            data.title
+          }. You will also need ${allIngredients.join(
+            ", "
+          )}. Would you like to proceed with this recipe or should I find a new recipe?`;
 
-					data.analyzedInstructions[0].steps.map((step) => {
-						this.setState({
-							recipeSteps : [ ...this.state.recipeSteps, step.step ]
-						});
-					});
-					this.setState({
-						results           : [ ...this.state.results, 'Kevin: ' + sentence ],
-						allIngredientsArr : allIngredients,
-						recipeImage       : data.image,
-						recipeTitle       : data.title
-					});
+          data.analyzedInstructions[0].steps.map((step) => {
+            this.setState({
+              recipeSteps: [...this.state.recipeSteps, step.step],
+            });
+          });
+          this.setState({
+            results: [...this.state.results, "Kevin: " + sentence],
+            allIngredientsArr: allIngredients,
+            recipeImage: data.image,
+            recipeTitle: data.title,
+          });
 
-					Tts.speak(sentence, this.state.ttsConfig);
-				}
-				else if (intent === 'recipeProceed') {
-					if (this.state.stepCount === 0) {
-						let instruction = this.state.recipeSteps[this.state.stepCount];
-						Tts.speak(instruction, this.state.ttsConfig);
-						this.setState({
-							results   : [ ...this.state.results, `Kevin: ${instruction}` ],
-							stepCount : this.state.stepCount + 1
-						});
-						this.props.navigation.navigate('Recipe', {
-							recipeSteps : [ ...this.state.recipeSteps ],
-							recipeImage : this.state.recipeImage,
-							recipeTitle : this.state.recipeTitle
-						});
-					}
-				}
-				else if (intent === 'newRecipe') {
-					//replace old recipe with new recipe
-					if (this.state.recipeCounter === 4) {
-						let lastRecipeMessage = 'Sorry, homie, there are no more recipes.';
-						Tts.speak(lastRecipeMessage, this.state.ttsConfig);
-						this.setState({
-							results : [ ...this.state.results, `Kevin: ${lastRecipeMessage}` ]
-						});
-					}
-					else {
-						this.setState({
-							recipeCounter : this.state.recipeCounter + 1,
-							recipeSteps   : []
-						});
-						const { data } = await spoonacularAPI(this.state.ingredientsArr, this.state.recipeCounter);
+          Tts.speak(sentence, this.state.ttsConfig);
+        } else if (intent === "recipeProceed") {
+          if (this.state.stepCount === 0) {
+            let instruction = this.state.recipeSteps[this.state.stepCount];
+            Tts.speak(instruction, this.state.ttsConfig);
+            this.setState({
+              results: [...this.state.results, `Kevin: ${instruction}`],
+              stepCount: this.state.stepCount + 1,
+            });
+            this.props.navigation.navigate("Recipe", {
+              recipeSteps: this.state.recipeSteps,
+              recipeImage: this.state.recipeImage,
+              recipeTitle: this.state.recipeTitle,
+            });
+          }
+        } else if (intent === "newRecipe") {
+          //replace old recipe with new recipe
+          if (this.state.recipeCounter === 4) {
+            let lastRecipeMessage = "Sorry, homie, there are no more recipes.";
+            Tts.speak(lastRecipeMessage, this.state.ttsConfig);
+            this.setState({
+              results: [...this.state.results, `Kevin: ${lastRecipeMessage}`],
+            });
+          } else {
+            this.setState({
+              recipeCounter: this.state.recipeCounter + 1,
+              recipeSteps: [],
+            });
+            const { data } = await spoonacularAPI(
+              this.state.ingredientsArr,
+              this.state.recipeCounter
+            );
 
-						let allIngredients = this.pullNames(data.extendedIngredients);
+            let allIngredients = this.pullNames(data.extendedIngredients);
 
-						let sentence = `I got a new recipe for ${data.title}. You will need ${allIngredients.join(
-							', '
-						)}. Would you like to proceed with this recipe or should I find another recipe?`;
+            let sentence = `I got a new recipe for ${
+              data.title
+            }. You will need ${allIngredients.join(
+              ", "
+            )}. Would you like to proceed with this recipe or should I find another recipe?`;
 
-						data.analyzedInstructions[0].steps.map((step) => {
-							this.setState({
-								recipeSteps : [ ...this.state.recipeSteps, step.step ]
-							});
-						});
-						this.setState({
-							results           : [ ...this.state.results, 'Kevin: ' + sentence ],
-							allIngredientsArr : allIngredients
-						});
-						Tts.speak(sentence, this.state.ttsConfig);
-					}
-				}
-				else if (intent === 'nextStep') {
-					if (this.state.stepCount === this.state.recipeSteps.length) {
-						let finishedMessage = 'You are finished. Good job. Enjoy the eats.';
-						Tts.speak(finishedMessage, this.state.ttsConfig);
-						this.setState({
-							results : [ ...this.state.results, `Kevin: ${finishedMessage}` ]
-						});
-					}
-					else {
-						let instruction = this.state.recipeSteps[this.state.stepCount];
-						Tts.speak(instruction, this.state.ttsConfig);
-						this.setState({
-							results   : [ ...this.state.results, `Kevin: ${instruction}` ],
-							stepCount : this.state.stepCount + 1
-						});
-					}
-				}
-				else {
-					// else if (intent === "repeatThat") {
-					//   let lastResponse = this.state.results[
-					//     this.state.results.length - 2
-					//   ].slice(6);
-					//   Tts.speak(`Sure, I said, ${lastResponse}`, this.state.ttsConfig);
-					//   this.setState({
-					//     results: [...this.state.results, `Kevin: ${lastResponse}`],
-					//   });
-					// }
-					console.log('running else statement');
-					this.setState({
-						results : [ ...this.state.results, 'Kevin: ' + response ]
-					});
-					Tts.speak(response, this.state.ttsConfig);
-				}
-			},
-			(error) => {
-				console.log(error);
-			}
-		);
-	}
+            data.analyzedInstructions[0].steps.map((step) => {
+              this.setState({
+                recipeSteps: [...this.state.recipeSteps, step.step],
+              });
+            });
+            this.setState({
+              results: [...this.state.results, "Kevin: " + sentence],
+              allIngredientsArr: allIngredients,
+            });
+            Tts.speak(sentence, this.state.ttsConfig);
+          }
+        } else if (intent === "nextStep") {
+          if (this.state.stepCount === this.state.recipeSteps.length) {
+            let finishedMessage = "You are finished. Good job. Enjoy the eats.";
+            Tts.speak(finishedMessage, this.state.ttsConfig);
+            this.setState({
+              results: [...this.state.results, `Kevin: ${finishedMessage}`],
+            });
+          } else {
+            let instruction = this.state.recipeSteps[this.state.stepCount];
+            Tts.speak(instruction, this.state.ttsConfig);
+            this.setState({
+              results: [...this.state.results, `Kevin: ${instruction}`],
+              stepCount: this.state.stepCount + 1,
+            });
+          }
+        } else {
+          // else if (intent === "repeatThat") {
+          //   let lastResponse = this.state.results[
+          //     this.state.results.length - 2
+          //   ].slice(6);
+          //   Tts.speak(`Sure, I said, ${lastResponse}`, this.state.ttsConfig);
+          //   this.setState({
+          //     results: [...this.state.results, `Kevin: ${lastResponse}`],
+          //   });
+          // }
+          console.log("running else statement");
+          this.setState({
+            results: [...this.state.results, "Kevin: " + response],
+          });
+          Tts.speak(response, this.state.ttsConfig);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
-	render() {
-		return (
-			<SafeAreaView style={styles.container}>
-				<Text style={styles.title1}>Kitchen Kevin</Text>
-				<Text style={styles.title2}>Conversation Text Log</Text>
-				<ScrollView
-					ref={(ref) => (this.ScrollView = ref)}
-					style={{ flex: 1 }}
-					contentContainerStyle={{
-						flexGrow       : 1,
-						justifyContent : 'space-around'
-					}}
-					onContentSizeChange={() => this.ScrollView.scrollToEnd({ animated: true })}
-				>
-					{this.state.results.map((result, idx) => (
-						<Text style={styles.textlog} key={idx}>
-							{result}
-						</Text>
-					))}
-				</ScrollView>
-				<Button
-					title="Talk To Kevin"
-					onPress={() => {
-						this.initiateConversation();
-					}}
-				/>
-				<Button
-					title="Stop Talking to Kevin"
-					onPress={() => {
-						Dialogflow_V2.finishListening();
-					}}
-				/>
-				{/* <Button title="tts-speak" onPress={this.handleTtsPress} /> */}
-				<StatusBar style="auto" />
-			</SafeAreaView>
-		);
-	}
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title1}>Kitchen Kevin</Text>
+        <Text style={styles.title2}>Conversation Text Log</Text>
+        <ScrollView
+          ref={(ref) => (this.ScrollView = ref)}
+          style={{ flex: 1 }}
+          contentContainerStyle={{
+            flexGrow: 1,
+            justifyContent: "space-around",
+          }}
+          onContentSizeChange={() =>
+            this.ScrollView.scrollToEnd({ animated: true })
+          }
+        >
+          {this.state.results.map((result, idx) => (
+            <Text style={styles.textlog} key={idx}>
+              {result}
+            </Text>
+          ))}
+        </ScrollView>
+        <Button
+          title="Talk To Kevin"
+          onPress={() => {
+            this.initiateConversation();
+          }}
+        />
+        <Button
+          title="Stop Talking to Kevin"
+          onPress={() => {
+            Dialogflow_V2.finishListening();
+          }}
+        />
+        <Button title="tts-stop" onPress={Tts.stop()} />
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container : {
-		flex            : 1,
-		backgroundColor : '#87CEFA',
-		alignItems      : 'center',
-		justifyContent  : 'center'
-	},
-	title1    : {
-		fontWeight : 'bold',
-		fontSize   : 30,
-		fontFamily : 'Chalkduster'
-	},
-	title2    : {
-		fontFamily : 'Chalkduster',
-		fontSize   : 25
-	},
-	textlog   : {
-		padding  : 6,
-		margin   : 10,
-		fontSize : 20,
-		color    : 'white'
-	}
+  container: {
+    flex: 1,
+    backgroundColor: "#87CEFA",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title1: {
+    fontWeight: "bold",
+    fontSize: 30,
+    fontFamily: "Chalkduster",
+  },
+  title2: {
+    fontFamily: "Chalkduster",
+    fontSize: 25,
+  },
+  textlog: {
+    padding: 6,
+    margin: 10,
+    fontSize: 20,
+    color: "white",
+  },
 });
--- Screens/RecipeScreen.js
+++ Screens/RecipeScreen.js
@@ -1,51 +1,74 @@
-import React, { Component } from 'react';
-import { View, Title, Image, Text, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-navigation';
+import React, { Component } from "react";
+import {
+  View,
+  Title,
+  Image,
+  Text,
+  StyleSheet,
+  List,
+  FlatList,
+} from "react-native";
+import { SafeAreaView } from "react-navigation";
 
 export default class RecipeScreen extends Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	render() {
-		const recipeSteps = this.props.navigation.getParam('recipeSteps', 'no-recipe');
-		const recipeImage = this.props.navigation.getParam('recipeImage', 'no-recipe-image');
-		const title = this.props.navigation.getParam('recipeTitle', 'no-title');
+  render() {
+    const recipeSteps = this.props.navigation.getParam(
+      "recipeSteps",
+      "no-recipe"
+    );
+    const recipeImage = this.props.navigation.getParam(
+      "recipeImage",
+      "no-recipe-image"
+    );
+    const title = this.props.navigation.getParam("recipeTitle", "no-title");
 
-		return (
-			<SafeAreaView style={styles.container}>
-				<Image
-					source={{
-						width  : 350,
-						height : 200,
-						uri    : recipeImage
-					}}
-				/>
-				<Text style={styles.title}>{title}</Text>
-				<Text style={styles.textBlock}>{recipeSteps}</Text>
-			</SafeAreaView>
-		);
-	}
+    console.log("RECIPE STEPS", recipeSteps);
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image
+          source={{
+            width: 350,
+            height: 200,
+            uri: recipeImage,
+          }}
+        />
+        <Text style={styles.title}>{title}</Text>
+        {/* <Text style={styles.textBlock}>{recipeSteps}</Text> */}
+        {/* {recipeSteps.map((step, index) => {
+          return <Text key={index}>{step}</Text>;
+        })} */}
+        <FlatList
+          data={recipeSteps}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Text>{`\u2022 ${item}`}</Text>}
+        />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container : {
-		flex            : 1,
-		backgroundColor : '#87CEFA',
-		alignItems      : 'center',
-		justifyContent  : 'center'
-	},
-	textBlock : {
-		margin          : 12,
-		padding         : 12,
-		borderWidth     : 1,
-		borderColor     : '#ccc',
-		backgroundColor : 'white'
-	},
-	title     : {
-		fontSize   : 24,
-		fontWeight : 'bold',
-		margin     : 12,
-		padding    : 12
-	}
+  container: {
+    flex: 1,
+    backgroundColor: "#87CEFA",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textBlock: {
+    margin: 12,
+    padding: 12,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    backgroundColor: "white",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    margin: 12,
+    padding: 12,
+  },
 });

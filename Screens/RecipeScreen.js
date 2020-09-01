4b6a511cc59e848706fc9852556a84efc60da5ea
Simon Zeng <zeng.simonl@gmail.com>
2020-09-01 15:49:29 -0400

    Added css styling to Recipe tab

--- Screens/RecipeScreen.js
+++ Screens/RecipeScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { View, Title, Image, Text, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-navigation';
+import { StatusBar, SafeAreaView } from 'react-native';
+// import { SafeAreaView } from 'react-navigation';
+import styled from 'styled-components';
+// import { StatusBar } from 'expo-status-bar';
 
 export default class RecipeScreen extends Component {
 	constructor(props) {
@@ -8,44 +10,97 @@ export default class RecipeScreen extends Component {
 	}
 
 	render() {
-		const recipeSteps = this.props.navigation.getParam('recipeSteps', 'no-recipe');
-		const recipeImage = this.props.navigation.getParam('recipeImage', 'no-recipe-image');
-		const title = this.props.navigation.getParam('recipeTitle', 'no-title');
+		const recipeSteps = this.props.navigation.getParam('recipeSteps');
+		const recipeImage = this.props.navigation.getParam('recipeImage');
+		const title = this.props.navigation.getParam('recipeTitle');
 
 		return (
-			<SafeAreaView style={styles.container}>
-				<Image
-					source={{
-						width  : 350,
-						height : 200,
-						uri    : recipeImage
-					}}
-				/>
-				<Text style={styles.title}>{title}</Text>
-				<Text style={styles.textBlock}>{recipeSteps}</Text>
-			</SafeAreaView>
+			<Container>
+				<StatusBar barStyle="light-content" />
+				<RecipeBackground source={{ uri: recipeImage }}>
+					<SafeAreaView>
+						<MainRecipe>
+							<Text>{title}</Text>
+							<Divider />
+						</MainRecipe>
+					</SafeAreaView>
+				</RecipeBackground>
+			</Container>
 		);
 	}
 }
 
-const styles = StyleSheet.create({
-	container : {
-		flex            : 1,
-		backgroundColor : '#87CEFA',
-		alignItems      : 'center',
-		justifyContent  : 'center'
-	},
-	textBlock : {
-		margin          : 12,
-		padding         : 12,
-		borderWidth     : 1,
-		borderColor     : '#ccc',
-		backgroundColor : 'white'
-	},
-	title     : {
-		fontSize   : 24,
-		fontWeight : 'bold',
-		margin     : 12,
-		padding    : 12
-	}
-});
+const Container = styled.View`
+	flex: 1;
+	background-color: #fff;
+`;
+
+const Text = styled.Text`
+	color: ${(props) => (props.dark ? '#000' : '#FFF')};
+	font-family: "AvenirNext-Regular";
+	fontSize: 20px;
+	fontWeight: 600;
+`;
+
+const RecipeBackground = styled.ImageBackground`width: 100%;`;
+
+const MenuBar = styled.View`
+	flex-direction: row;
+	justify-content: space-between;
+	padding: 16px;
+`;
+
+const Back = styled.View`
+	flex-direction: row;
+	align-items: center;
+`;
+
+const MainRecipe = styled.View`
+	padding: 0 32px;
+	margin: 200px 0 32px 0;
+`;
+
+const Divider = styled.View`
+	border-bottom-color: #fff;
+	border-bottom-width: 2px;
+	width: 180px;
+	margin: 8px 0;
+`;
+
+// 		return (
+// 			<SafeAreaView style={styles.container}>
+// 				<Image
+// 					source={{
+// 						width  : 350,
+// 						height : 200,
+// 						uri    : recipeImage
+// 					}}
+// 				/>
+// 				<Text style={styles.title}>{title}</Text>
+// 				<Text style={styles.textBlock}>{recipeSteps}</Text>
+// 			</SafeAreaView>
+// 		);
+// 	}
+// }
+
+// const styles = StyleSheet.create({
+// 	container : {
+// 		flex            : 1,
+// 		backgroundColor : '#87CEFA',
+// 		alignItems      : 'center',
+// 		justifyContent  : 'center'
+// 	},
+// 	textBlock : {
+// 		margin  : 12,
+// 		padding : 12
+// 		// borderWidth     : 1,
+// 		// borderColor     : '#ccc',
+// 		// backgroundColor : 'white'
+// 	},
+// 	title     : {
+// 		fontSize   : 24,
+// 		fontWeight : 'bold',
+// 		margin     : 12,
+// 		padding    : 12
+// 	}
+// });
